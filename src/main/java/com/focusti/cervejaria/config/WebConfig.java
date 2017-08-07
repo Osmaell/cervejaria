54ac54615ab3a14fed87543650cdd7e33240956b
root <osmaelsousa.ti@gmail.com>
2017-08-07 20:37:43 -0300

    Criação de componente de erro e verificação de campo

--- src/main/java/com/focusti/cervejaria/config/WebConfig.java
+++ src/main/java/com/focusti/cervejaria/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.focusti.cervejaria.config;
 import java.math.BigDecimal;
 
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.format.FormatterRegistry;
@@ -10,11 +11,18 @@ import org.springframework.format.number.NumberStyleFormatter;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+import com.focusti.cervejaria.thymeleaf.BrewerDialect;
+
 @Configuration
 @EnableSpringDataWebSupport
 @EnableCaching
 @EnableAsync
 public class WebConfig extends WebMvcConfigurerAdapter {
+
+	@Bean
+	public BrewerDialect brewerDialect() {
+		return new BrewerDialect();
+	}
 	
 	@Override
 	public void addFormatters(FormatterRegistry registry) {
