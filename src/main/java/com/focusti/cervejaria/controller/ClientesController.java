32ae35fed91eb1dc125381f248be99f74aa8a008
root <osmaelsousa.ti@gmail.com>
2017-08-28 22:24:56 -0300

    Implementando radio tipo pessoa

--- src/main/java/com/focusti/cervejaria/controller/ClientesController.java
+++ src/main/java/com/focusti/cervejaria/controller/ClientesController.java
@@ -3,14 +3,21 @@ package com.focusti.cervejaria.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.focusti.cervejaria.model.constants.TipoPessoa;
 
 @Controller
 @RequestMapping("/clientes")
 public class ClientesController {
 	
 	@GetMapping("/novo")
-	public String novo() {
-		return "cliente/CadastroCliente";
+	public ModelAndView novo() {
+		
+		ModelAndView mv = new ModelAndView("cliente/CadastroCliente");
+		mv.addObject("tiposPessoa", TipoPessoa.values());
+		
+		return mv;
 	}
 	
 }
