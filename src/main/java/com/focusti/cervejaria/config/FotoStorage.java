d823181003dd5fb26088741035d9a29bd047a13f
Osmael <osmaelsousa.ti@gmail.com>
2018-01-25 09:39:05 -0300

    Enviando o e-mail em html com imagens

--- src/main/java/com/focusti/cervejaria/config/FotoStorage.java
+++ src/main/java/com/focusti/cervejaria/config/FotoStorage.java
@@ -1,11 +1,11 @@
 package com.focusti.cervejaria.config;
-
+	
 import org.springframework.web.multipart.MultipartFile;
-
+	
 public interface FotoStorage {
 	
 	public String salvarTemporariamente(MultipartFile ...files);
-
+	
 	public byte[] recuperarFotoTemporaria(String nome);
 	
 }
--- src/main/java/com/focusti/cervejaria/mail/Mailer.java
+++ src/main/java/com/focusti/cervejaria/mail/Mailer.java
@@ -1,30 +1,60 @@
 package com.focusti.cervejaria.mail;
 	
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import com.focusti.cervejaria.model.Venda;
 	
 @Component
 public class Mailer {
 	
+	private static Logger logger = LoggerFactory.getLogger(Mailer.class);
+	
 	@Autowired
 	private JavaMailSender mailSender;
+	
+	@Autowired
+	private TemplateEngine thymeleaf;
 	
 	@Async
 	public void enviar(Venda venda) {
 		
-		SimpleMailMessage mensagem = new SimpleMailMessage();
+		Context context = new Context();
+		context.setVariable("venda", venda);
+		context.setVariable("logo", "logo");
+		context.setVariable("mockCerveja", "mockCerveja");
 		
-		mensagem.setFrom("osmaelsousabraga@gmail.com");
-		mensagem.setTo(venda.getCliente().getEmail());
-		mensagem.setSubject("Venda efetuada");
-		mensagem.setText("Obrigado, sua venda foi processada!");
-		
-		mailSender.send(mensagem);
+		try {
+			
+			String email = thymeleaf.process("mail/ResumoVenda", context);
+			
+			MimeMessage mimeMessage = mailSender.createMimeMessage();
+			MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+			
+			helper.setFrom("osmaeltestefrom@gmail.com");
+			helper.setTo("osmaeltesteto@gmail.com");
+			helper.setSubject("Brewer - Venda realizada");
+			helper.setText(email, true);
+			
+			helper.addInline("logo", new ClassPathResource("static/images/logo-gray.png"));
+			helper.addInline("mockCerveja", new ClassPathResource("static/images/cerveja-mock.png"));
+			
+			mailSender.send(mimeMessage);
+			
+		} catch (MessagingException e) {
+			logger.error("Erro enviando e-mail", e);
+		}
 		
 	}
 	
