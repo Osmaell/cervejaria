75ca062481a31286e3fa3e17047708365a89a2ae
Osmael <osmaelsousa.ti@gmail.com>
2018-01-29 23:44:07 -0300

    Exclusão de cerveja

--- src/main/java/com/focusti/cervejaria/controller/CervejasController.java
+++ src/main/java/com/focusti/cervejaria/controller/CervejasController.java
@@ -10,9 +10,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -28,6 +31,7 @@ import com.focusti.cervejaria.repository.Cervejas;
 import com.focusti.cervejaria.repository.Estilos;
 import com.focusti.cervejaria.repository.filter.CervejaFilter;
 import com.focusti.cervejaria.service.CervejaService;
+import com.focusti.cervejaria.service.exception.ImpossivelExcluirEntidadeException;
 	
 @Controller
 @RequestMapping("/cervejas")
@@ -88,6 +92,18 @@ public class CervejasController {
 	@GetMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody List<CervejaDTO> pesquisar(String skuOuNome) {
 		return cervejas.porSkuOuNome(skuOuNome);
+	}
+	
+	@DeleteMapping("/{codigo}")
+	public @ResponseBody ResponseEntity<?> excluir( @PathVariable Long codigo ) {
+		
+		try {
+			cervejaService.excluir(codigo);
+		} catch (ImpossivelExcluirEntidadeException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+		
+		return ResponseEntity.ok().build();
 	}
 	
 }
--- src/main/java/com/focusti/cervejaria/service/CervejaService.java
+++ src/main/java/com/focusti/cervejaria/service/CervejaService.java
@@ -1,10 +1,12 @@
 package com.focusti.cervejaria.service;
 	
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.focusti.cervejaria.model.Cerveja;
 import com.focusti.cervejaria.repository.Cervejas;
+import com.focusti.cervejaria.service.exception.ImpossivelExcluirEntidadeException;
 	
 @Service
 public class CervejaService {
@@ -14,6 +16,17 @@ public class CervejaService {
 	
 	public void salvar(Cerveja cerveja) {
 		cervejas.save(cerveja);
+	}
+	
+	public void excluir(Long codigo) {
+		
+		try {
+			cervejas.delete(codigo);
+			cervejas.flush();
+		} catch (DataIntegrityViolationException e) {
+			throw new ImpossivelExcluirEntidadeException("Impossível apagar cerveja. Já foi usada em alguma venda.");
+		}
+		
 	}
 	
 }
